4f9fa7794d7412733cce91dd0509e736be68a170
Aman Jain <145151053+itzamanjain@users.noreply.github.com>
2024-11-05 11:58:59 +0530

    add ratelimit
    before add test it once

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,8 +2,48 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY!);
 
+const rateLimitStore = new Map();
+const RATE_LIMIT_TIME_FRAME = 15 * 60 * 1000; // 15 minutes
+const RATE_LIMIT_MAX_REQUESTS = 3; // Max 3 requests per IP per time frame
+
+function rateLimit(request:NextRequest) {
+    const ip = request.headers.get('x-forwarded-for') || request.ip ;
+    console.log('IP:', ip);
+    
+    const now = Date.now();
+
+    if (!rateLimitStore.has(ip)) {
+        rateLimitStore.set(ip, { count: 1, lastRequest: now });
+    } else {
+        const rateLimitData = rateLimitStore.get(ip);
+        if (now - rateLimitData.lastRequest > RATE_LIMIT_TIME_FRAME) {
+            // Reset the rate limit for the IP if the time frame has passed
+            rateLimitStore.set(ip, { count: 1, lastRequest: now });
+        } else {
+            // Increment the request count for the IP
+            rateLimitData.count++;
+            rateLimitData.lastRequest = now;
+
+            if (rateLimitData.count > RATE_LIMIT_MAX_REQUESTS) {
+                return false; // Rate limit exceeded
+            }
+        }
+    }
+
+    return true; // Within the rate limit
+}
+
+
+
 export async function POST(request: Request) {
   try {
+    if (!rateLimit(request)) {
+            return NextResponse.json({
+                message: 'Too many requests. Please try again later.',
+                success: false
+            }, { status: 429 });
+        }
+    
     const formData = await request.formData();
     const description = formData.get("description") as string;
     const url = formData.get("url") as string;
