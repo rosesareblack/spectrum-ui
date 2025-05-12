761177181f35bbd08a70c6f698b5c07e6be2e5c7
Arihant Jain <jainari1208@gmail.com>
2025-05-12 23:15:46 +0530

    refactor: clean up page component and remove unused testing file

--- app/(blocks)/blocks/page.tsx
+++ app/(blocks)/blocks/page.tsx
@@ -3,20 +3,16 @@ import PreviewCodeCard from "@/app/(docs)/docs/components/preview-code-card";
 import React from "react";
 import Dashboardblock from "./dashboard";
 
-
-
 const page = () => {
   return (
     <div className="max-w-7xl mx-auto container-wrapper px-8 py-2">
       <PageTemplate title="Pharmalogistics Dashboard" className="mt-5">
         <PreviewCodeCard
-        className="p-4"
-        path="app/(blocks)/blocks/dashboard.tsx">
-          <Dashboardblock/>
+          className="p-4"
+          path="app/(blocks)/blocks/dashboard.tsx"
+        >
+          <Dashboardblock />
         </PreviewCodeCard>
-
-      
-        
       </PageTemplate>
     </div>
   );
