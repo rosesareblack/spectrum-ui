813f7210ae732203f32c883d8de3fa5fae708663
Arihant Jain <jainari1208@gmail.com>
2025-06-20 16:26:37 +0530

    fix the bug

--- components/provider.tsx
+++ components/provider.tsx
@@ -1,10 +1,6 @@
-// app/providers.tsx
 'use client'
 
-import { usePathname, useSearchParams } from "next/navigation"
 import { useEffect } from "react"
-import { usePostHog } from 'posthog-js/react'
-
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
 
@@ -12,8 +8,9 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-      defaults: '2025-05-24'
+      person_profiles: 'identified_only',
+      capture_pageview: true,
+      capture_pageleave: true,
     })
   }, [])
 
